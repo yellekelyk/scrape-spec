1783458759eea3212a136d9cfe77c3981579e97f
Kyle Kelley <kkelley@stanford.edu>
2012-07-24 12:58:51 -0700

    updated 1995 test names to work: '_' --> '.'

--- SpecDataElemInt1995.py
+++ SpecDataElemInt1995.py
@@ -4,23 +4,23 @@ class SpecDataElemInt1995(SpecDataElem1995):
     "A class that holds spec int data for a given processor"
 
     def __init__(self):
-        tests = ["099_go",
-                 "124_m88ksim",
-                 "126_gcc",
-                 "129_compress",
-                 "130_li",
-                 "132_ijpeg",
-                 "134_perl", 
-                 "147_vortex"] 
+        tests = ["099.go",
+                 "124.m88ksim",
+                 "126.gcc",
+                 "129.compress",
+                 "130.li",
+                 "132.ijpeg",
+                 "134.perl", 
+                 "147.vortex"] 
 
-        attrMap = {"099 Base": "099_go",
-                   "124 Base": "124_m88ksim",
-                   "126 Base": "126_gcc",
-                   "129 Base": "129_compress",
-                   "130 Base": "130_li",
-                   "132 Base": "132_ijpeg",
-                   "134 Base": "134_perl",
-                   "147 Base": "147_vortex"}
+        attrMap = {"099 Base": "099.go",
+                   "124 Base": "124.m88ksim",
+                   "126 Base": "126.gcc",
+                   "129 Base": "129.compress",
+                   "130 Base": "130.li",
+                   "132 Base": "132.ijpeg",
+                   "134 Base": "134.perl",
+                   "147 Base": "147.vortex"}
 
         SpecDataElem1995.__init__(self, tests=tests, attrMap=attrMap)
 
